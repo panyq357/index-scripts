3a9f90c7a896f84d7498e09f4cfa9cc06dd9847c
panyq357 <panyq357@qq.com>
2023-10-10 10:18:16 +0800

    os/rap-db/2023-10-10

---
+++ os/rap-db/2023-10-10/scripts/make_gtf.R
@@ -0,0 +1,69 @@
+library(rtracklayer)
+library(GenomicRanges)
+
+config <- list(
+    gff = "rawdata/IRGSP-1.0_representative/transcripts.gff",
+    outdir = "gtf",
+    out_gtf = "os.rap-db.make.gtf"
+)
+
+if (!dir.exists(config$outdir)) dir.create(config$outdir)
+
+tx_gff <- rtracklayer::import(config$gff)
+
+transcript <- subset(tx_gff, type == "mRNA")
+transcript$gene_id <- transcript$Locus_id
+transcript$transcript_id <- transcript$ID
+transcript$type <- "transcript"
+transcript <- subset(transcript, select = c("source", "type", "score", "phase", "gene_id", "transcript_id"))
+
+CDS <- subset(tx_gff, type == "CDS")
+CDS$transcript_id <- CDS$Parent
+CDS$gene_id <- sub("Os(\\d{2})t(\\d{7}).*", "Os\\1g\\2", CDS$transcript_id)
+CDS <- subset(CDS, select = c("source", "type", "score", "phase", "gene_id", "transcript_id"))
+
+five_prime_utr <- subset(tx_gff, type == "five_prime_UTR")
+five_prime_utr$transcript_id <- five_prime_utr$Parent
+five_prime_utr$gene_id <- sub("Os(\\d{2})t(\\d{7}).*", "Os\\1g\\2", five_prime_utr$transcript_id)
+five_prime_utr$type <- "five_prime_utr"
+five_prime_utr <- subset(five_prime_utr, select = c("source", "type", "score", "phase", "gene_id", "transcript_id"))
+
+three_prime_utr <- subset(tx_gff, type == "three_prime_UTR")
+three_prime_utr$transcript_id <- three_prime_utr$Parent
+three_prime_utr$gene_id <- sub("Os(\\d{2})t(\\d{7}).*", "Os\\1g\\2", three_prime_utr$transcript_id)
+three_prime_utr$type <- "three_prime_utr"
+three_prime_utr <- subset(three_prime_utr, select = c("source", "type", "score", "phase", "gene_id", "transcript_id"))
+
+exon <- c(CDS, five_prime_utr, three_prime_utr)
+exon_by_tx <- split(exon, as.character(exon$transcript_id))
+exon_by_tx <- reduce(exon_by_tx)
+exon <- unlist(exon_by_tx)
+exon$transcript_id <- rep(names(exon_by_tx), elementNROWS(exon_by_tx))
+exon$gene_id <- sub("Os(\\d{2})t(\\d{7}).*", "Os\\1g\\2", exon$transcript_id)
+exon$source <- "calculated"
+exon$type <- "exon"
+exon$phase <- NA
+exon$score <- NA
+names(exon) <- NULL
+
+# tx_by_gene <- split(transcript, as.character(transcript$gene_id))
+# gene <- unlist(reduce(tx_by_gene))
+# gene$gene_id <- names(gene)
+# names(gene) <- NULL
+# gene$transcript_id <- NA
+# gene$source <- "calculated"
+# gene$type <- "gene"
+# gene$phase <- NA
+# gene$score <- NA
+# 
+# new_gtf <- c(transcript, CDS, five_prime_utr, three_prime_utr, exon, gene)
+
+new_gtf <- c(transcript, CDS, five_prime_utr, three_prime_utr, exon)
+
+seqlevels(new_gtf) <- sprintf("chr%02d", 1:12)
+new_gtf <- sort(new_gtf, by = ~ width, decreasing=T)
+new_gtf <- sort(new_gtf, by = ~ start, decreasing=F)
+new_gtf <- sort(new_gtf, by = ~ seqnames, decreasing=F)
+
+export(new_gtf, file.path(config$outdir, config$out_gtf), format="gtf")
+
