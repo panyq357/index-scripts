4edcd97b3a8a075416c296716ecf340ec32bc542
panyq357 <panyq357@qq.com>
2025-01-16 11:04:41 +0800

    fix custom gene.bed

--- at/tair/workflow/scripts/custom_bed.R
+++ at/tair/workflow/scripts/custom_bed.R
@@ -1,28 +1,15 @@
 library(rtracklayer)
 library(GenomicRanges)
 
-config <- list(
-    gtf = snakemake@input$gtf,
+gtf <- import(snakemake@input$gtf)
 
-    transcript = snakemake@output$transcript,
-    gene = snakemake@output$gene
-)
+transcript <- gtf[gtf$type == "transcript",]
+mcols(transcript)$name <- transcript$transcript_id
+mcols(transcript) <- mcols(transcript)["name"]  # Keep only name in mcols, since rtracklayer can't export other columns.
 
-main <- function() {
+gene_range_list <- split(gtf, gtf$gene_id) |> range()
+gene <- unlist(gene_range_list)
+gene$name <- names(gene_range_list)
 
-    gtf <- import(config$gtf)
-
-    transcript <- gtf[gtf$type == "transcript",]
-    mcols(transcript)$name <- transcript$transcript_id
-    mcols(transcript) <- mcols(transcript)["name"]  # Keep only name in mcols, since rtracklayer can't export other columns.
-
-    gene <- gtf[gtf$type == "gene",]
-    mcols(gene)$name <- gene$gene_id
-    mcols(gene) <- mcols(gene)["name"]
-
-    export(transcript, config$transcript, format="bed")
-    export(gene, config$gene, format="bed")
-
-}
-
-main()
+export(transcript, snakemake@output$transcript, format="bed")
+export(gene, snakemake@output$gene, format="bed")
