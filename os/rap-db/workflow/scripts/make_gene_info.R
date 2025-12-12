e3cb223c2527be47d3232528144ab648c3035959
panyq357 <panyq357@qq.com>
2025-12-12 20:27:25 +0800

    remove duplicated rows in gene_info

--- os/rap-db/workflow/scripts/make_gene_info.R
+++ os/rap-db/workflow/scripts/make_gene_info.R
@@ -1,2 +1,6 @@
-readr::read_tsv(snakemake@input[[1]])[c("Locus_ID", "Oryzabase Gene Symbol Synonym(s)", "Description")] |>
-  readr::write_tsv(snakemake@output[[1]], col_names=F)
+df <- readr::read_tsv(snakemake@input[[1]])[c("Locus_ID", "Oryzabase Gene Symbol Synonym(s)", "Description")]
+df <- dplyr::distinct(df)
+
+df <- df[match(unique(df[[1]]), df[[1]]), ]
+
+readr::write_tsv(df, snakemake@output[[1]], col_names=FALSE)
